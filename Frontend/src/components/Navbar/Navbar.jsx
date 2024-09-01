9d9690df212102af694aeba4dd50993e4a9c24ab
JAYANTJOSHI001 <jayantjoshi909@gmail.com>
2024-09-01 11:36:11 +0530

    Made pages responsive and added animations
    Signed-off-by: JAYANTJOSHI001 <jayantjoshi909@gmail.com>

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { Box, Flex, Image, IconButton, Collapse, VStack, HStack } from "@chakra-ui/react";
+import { Box, Flex, Image, Icon, Collapse, VStack, HStack } from "@chakra-ui/react";
 import logo from "../../assets/logo.jpg";
 import { useNavigate } from "react-router-dom";
 
@@ -13,24 +13,47 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const clickHome = () => {
+    navigate("/");
+  };
+
   const clickAbout = () => {
     navigate("/about");
+  };
+
+  const clickAchievement = () => {
+    navigate("/achievements");
+  };
+
+  const clickGallery = () => {
+    navigate("/gallery");
+  };
+
+  const clickSponsor = () => {
+    navigate("/sponsor");
+  };
+
+  const clickTeam= () => {
+    navigate("/team");
+  };
+
+  const clickcontact = () => {
+    navigate("/contact");
   };
 
   return (
     <VStack>
     <HStack bg="black" color="white" position="fixed" w="100%" zIndex={3} justify="center">
       <Flex justify="space-between" align="center" p={4} w="90%">
-        <Image h="60px" src={logo} alt="Logo" />
-
-        <IconButton
-          display={{ base: "block", md:"block" ,lg: "none" }}
+        <Image h="60px" onClick={clickHome} src={logo} alt="Logo"/>
+        <Icon 
+          as={GiHamburgerMenu} 
           onClick={toggleMenu}
-          icon={<GiHamburgerMenu />}
-          color="red.800"
-          variant="outline"
+          display={{ base: "block", md:"block" ,lg: "none" }}
+          w={8} 
+          h={8} 
           aria-label="Toggle Navigation"
-        />
+          color='red.500' />
 
         <Flex
           as="nav"
@@ -40,7 +63,8 @@ const Navbar = () => {
         >
           <Box
             px="7px"
-            _hover={{ color: "red" }}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            onClick={clickHome}
             border={location === "/" ? "2px solid red" : "none"}
             borderRadius={location === "/" ? "30px" : "none"}
           >
@@ -48,7 +72,7 @@ const Navbar = () => {
           </Box>
           <Box
             px="7px"
-            _hover={{ color: "red" }}
+            _hover={{ color: "red" , cursor:"pointer"}}
             onClick={clickAbout}
             border={location === "/about" ? "2px solid red" : "none"}
             borderRadius={location === "/about" ? "30px" : "none"}
@@ -57,7 +81,8 @@ const Navbar = () => {
           </Box>
           <Box
             px="7px"
-            _hover={{ color: "red" }}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            onClick={clickAchievement}
             border={location === "/achievements" ? "2px solid red" : "none"}
             borderRadius={location === "/achievements" ? "30px" : "none"}
           >
@@ -65,7 +90,8 @@ const Navbar = () => {
           </Box>
           <Box
             px="7px"
-            _hover={{ color: "red" }}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            onClick={clickGallery}
             border={location === "/gallery" ? "2px solid red" : "none"}
             borderRadius={location === "/gallery" ? "30px" : "none"}
           >
@@ -73,7 +99,8 @@ const Navbar = () => {
           </Box>
           <Box
             px="7px"
-            _hover={{ color: "red" }}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            onClick={clickTeam}
             border={location === "/team" ? "2px solid red" : "none"}
             borderRadius={location === "/team" ? "30px" : "none"}
           >
@@ -81,7 +108,8 @@ const Navbar = () => {
           </Box>
           <Box
             px="7px"
-            _hover={{ color: "red" }}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            onClick={clickSponsor}
             border={location === "/sponsors" ? "2px solid red" : "none"}
             borderRadius={location === "/sponsors" ? "30px" : "none"}
           >
@@ -89,7 +117,8 @@ const Navbar = () => {
           </Box>
           <Box
             px="7px"
-            _hover={{ color: "red" }}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            onClick={clickcontact}
             border={location === "/contact" ? "2px solid red" : "none"}
             borderRadius={location === "/contact" ? "30px" : "none"}
           >
@@ -98,7 +127,7 @@ const Navbar = () => {
         </Flex>
       </Flex>
       </HStack>
-      <Collapse in={isOpen} animateOpacity w="100%">
+      <Box display={isOpen? "block" : "none"} animateOpacity w="100%"  pt="11vh">
         <VStack
           bg="black"
           color="white"
@@ -107,66 +136,72 @@ const Navbar = () => {
           spacing={4}
           align="stretch"
           w="100%"
-          border="2px solid red"
         >
           <Box
             onClick={() => navigate("/")}
-            _hover={{ color: "red" }}
+            _hover={{ color: "red" , cursor:"pointer"}}
             border={location === "/" ? "2px solid red" : "none"}
             borderRadius={location === "/" ? "30px" : "none"}
+            p="5px"
           >
             HOME
           </Box>
           <Box
             onClick={clickAbout}
-            _hover={{ color: "red" }}
+            _hover={{ color: "red" , cursor:"pointer"}}
             border={location === "/about" ? "2px solid red" : "none"}
             borderRadius={location === "/about" ? "30px" : "none"}
+            p="5px"
           >
             ABOUT US
           </Box>
           <Box
-            onClick={() => navigate("/achievements")}
-            _hover={{ color: "red" }}
+            onClick={clickAchievement}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            p="5px"
             border={location === "/achievements" ? "2px solid red" : "none"}
             borderRadius={location === "/achievements" ? "30px" : "none"}
           >
             ACHIEVEMENTS
           </Box>
           <Box
-            onClick={() => navigate("/gallery")}
-            _hover={{ color: "red" }}
+            onClick={clickGallery}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            p="5px"
             border={location === "/gallery" ? "2px solid red" : "none"}
             borderRadius={location === "/gallery" ? "30px" : "none"}
           >
             GALLERY
           </Box>
           <Box
-            onClick={() => navigate("/team")}
-            _hover={{ color: "red" }}
+            onClick={clickTeam}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            p="5px"
             border={location === "/team" ? "2px solid red" : "none"}
             borderRadius={location === "/team" ? "30px" : "none"}
           >
             OUR TEAM
           </Box>
           <Box
-            onClick={() => navigate("/sponsors")}
-            _hover={{ color: "red" }}
+            onClick={clickSponsor}
+            _hover={{ color: "red" , cursor:"pointer"}}
+            p="5px"
             border={location === "/sponsors" ? "2px solid red" : "none"}
             borderRadius={location === "/sponsors" ? "30px" : "none"}
           >
             SPONSOR US
           </Box>
           <Box
-            onClick={() => navigate("/contact")}
-            _hover={{ color: "red" }}
+            onClick={clickcontact}
+            _hover={{ color:"red" , cursor:"pointer"}}
+            p="5px"
             border={location === "/contact" ? "2px solid red" : "none"}
             borderRadius={location === "/contact" ? "30px" : "none"}
           >
             CONTACT US
           </Box>
         </VStack>
-      </Collapse>
+      </Box>
     </VStack>
   );
 };
---
+++ Frontend/src/pages/Contact Us/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+
+  return (
+    <Box>
+      
+    </Box>
+  );
+};
+
+export default Contact;
